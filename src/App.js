d34f62a54916482a37395686e759032a27d79ad6
anthonyBgood <anthonyBpostbox@gmail.com>
2019-03-21 14:38:26 +1000

    moved search to a compoent

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react'
 // import * as BooksAPI from './BooksAPI'
 import './App.css'
 
+import SearchBooks from './SearchBooks';
+
 class BooksApp extends React.Component {
   state = {
     /**
@@ -13,29 +15,25 @@ class BooksApp extends React.Component {
     showSearchPage: false
   }
 
+
+
+
   render() {
+
+    const toggleSearchPage = () => {
+      this.setState(() => ({showSearchPage: false}));
+    }
+
+
+
+
     return (
       <div className="app">
         {this.state.showSearchPage ? (
-          <div className="search-books">
-            <div className="search-books-bar">
-              <button className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</button>
-              <div className="search-books-input-wrapper">
-                {/*
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
+          <div>
+            <SearchBooks toggleSearchPage = {toggleSearchPage}/>
 
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
-                <input type="text" placeholder="Search by title or author"/>
 
-              </div>
-            </div>
-            <div className="search-books-results">
-              <ol className="books-grid"></ol>
-            </div>
           </div>
         ) : (
           <div className="list-books">
